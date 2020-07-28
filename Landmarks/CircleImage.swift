cc5654ec382e1d451ab132eade7f808d5560edbe
Kuba Suder <jakub.suder@gmail.com>
2020-07-28 17:04:12 +0200

    created landmark image circle view

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,24 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Kuba Suder on 28.07.2020.
+//  Copyright © 2020 Kuba Suder. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10.0)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
