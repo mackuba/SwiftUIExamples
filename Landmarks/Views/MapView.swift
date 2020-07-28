0d7656dd99c06f532b8417ccdcb44a3b7f431731
Kuba Suder <jakub.suder@gmail.com>
2020-07-28 22:38:57 +0200

    navigating to detail view + showing real data

--- Landmarks/Views/MapView.swift
+++ Landmarks/Views/MapView.swift
@@ -10,12 +10,13 @@ import MapKit
 import SwiftUI
 
 struct MapView: UIViewRepresentable {
+    var coordinate: CLLocationCoordinate2D
+
     func makeUIView(context: Context) -> MKMapView {
         return MKMapView(frame: .zero)
     }
 
     func updateUIView(_ uiView: MKMapView, context: Context) {
-        let coordinate = CLLocationCoordinate2D(latitude: 34.011286, longitude: -116.166868)
         let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
         let region = MKCoordinateRegion(center: coordinate, span: span)
 
@@ -25,6 +26,7 @@ struct MapView: UIViewRepresentable {
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView()
+        let coordinate = landmarkData[0].locationCoordinate
+        return MapView(coordinate: coordinate)
     }
 }
