24bd6d1a7d154baac68bc22a3d28bf31540fcf52
Kuba Suder <jakub.suder@gmail.com>
2020-07-28 15:55:33 +0200

    created a new iOS app project

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Kuba Suder on 28.07.2020.
+//  Copyright © 2020 Kuba Suder. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
