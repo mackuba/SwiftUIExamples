c905d0aa63163308fd22210571cca79f95383bd2
Kuba Suder <jakub.suder@gmail.com>
2020-07-29 21:39:25 +0200

    showing favorites on the list

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -9,11 +9,20 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @State var showFavoritesOnly = true
+
     var body: some View {
         NavigationView {
-            List(landmarkData) { landmark in
-                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
-                    LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites Only")
+                }
+                ForEach(landmarkData) { landmark in
+                    if landmark.isFavorite || !self.showFavoritesOnly {
+                        NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                            LandmarkRow(landmark: landmark)
+                        }
+                    }
                 }
             }
             .navigationBarTitle(Text("Landmarks"), displayMode: .inline)
