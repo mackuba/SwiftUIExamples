d6afc7b3a3df9115bd7b28e4d4a91c5352c4754d
Kuba Suder <jakub.suder@gmail.com>
2020-08-12 20:56:06 +0200

    added Mac app target

---
+++ MacLandmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MacLandmarks
+//
+//  Created by Kuba Suder on 12.08.2020.
+//  Copyright © 2020 Kuba Suder. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
