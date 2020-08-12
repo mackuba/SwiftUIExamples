ed6a3a8a4f02423ec81a1b0714158c4e2ae3f5d9
Kuba Suder <jakub.suder@gmail.com>
2020-08-12 23:37:21 +0200

    implemented sidebar view with list & filter

--- MacLandmarks/LandmarkList.swift
+++ MacLandmarks/LandmarkList.swift
@@ -11,11 +11,24 @@ import SwiftUI
 struct LandmarkList: View {
     @EnvironmentObject private var userData: UserData
     @Binding var selectedLandmark: Landmark?
+    @Binding var filter: FilterType
+
+    func matchesFavoriteFilter(_ landmark: Landmark) -> Bool {
+        return landmark.isFavorite || !self.userData.showFavoritesOnly
+    }
+
+    func matchesCategoryFilter(_ landmark: Landmark) -> Bool {
+        return self.filter == .all
+            || self.filter.category == landmark.category
+            || (self.filter.category == .featured && landmark.isFeatured)
+    }
 
     var body: some View {
         List(selection: $selectedLandmark) {
             ForEach(userData.landmarks) { landmark in
-                LandmarkRow(landmark: landmark).tag(landmark)
+                if self.matchesFavoriteFilter(landmark) && self.matchesCategoryFilter(landmark) {
+                    LandmarkRow(landmark: landmark).tag(landmark)
+                }
             }
         }
     }
@@ -23,7 +36,10 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList(selectedLandmark: .constant(landmarkData[0]))
-            .environmentObject(UserData())
+        LandmarkList(
+            selectedLandmark: .constant(landmarkData[0]),
+            filter: .constant(.all)
+        )
+        .environmentObject(UserData())
     }
 }
---
+++ MacLandmarks/NavigationMaster.swift
@@ -0,0 +1,34 @@
+//
+//  NavigationMaster.swift
+//  MacLandmarks
+//
+//  Created by Kuba Suder on 12.08.2020.
+//  Copyright © 2020 Kuba Suder. All rights reserved.
+//
+
+import SwiftUI
+
+struct NavigationMaster: View {
+    @Binding var selectedLandmark: Landmark?
+    @State private var filter: FilterType = .all
+
+    var body: some View {
+        VStack {
+            Filter(filter: $filter)
+                .controlSize(.small)
+                .padding([.top, .leading], 8)
+                .padding(.trailing, 4)
+
+            LandmarkList(selectedLandmark: $selectedLandmark, filter: $filter)
+                .listStyle(SidebarListStyle())
+        }
+        .frame(minWidth: 225, maxWidth: 300)
+    }
+}
+
+struct NavigationMaster_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationMaster(selectedLandmark: .constant(landmarkData[1]))
+            .environmentObject(UserData())
+    }
+}
