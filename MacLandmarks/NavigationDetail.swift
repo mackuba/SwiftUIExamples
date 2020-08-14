8b9312607447143861ffaa920a7ade4171d045c4
Kuba Suder <jakub.suder@gmail.com>
2020-08-14 17:15:40 +0200

    refactoring & visual tweaks

--- MacLandmarks/NavigationDetail.swift
+++ MacLandmarks/NavigationDetail.swift
@@ -17,19 +17,55 @@ struct NavigationDetail: View {
         userData.landmarks.firstIndex(where: { $0.id == landmark.id })!
     }
 
+    var landmarkIsFavorite: Bool {
+        return userData.landmarks[self.landmarkIndex].isFavorite
+    }
+
+    var openInMapsButton: some View {
+        Button("Open in Maps") {
+            let destination = MKMapItem(placemark: MKPlacemark(coordinate: self.landmark.locationCoordinate))
+            destination.name = self.landmark.name
+            destination.openInMaps()
+        }
+    }
+
+    var favoriteButton: some View {
+        Button(action: self.toggleFavoriteStatus) {
+            if self.landmarkIsFavorite {
+                Image("star-filled")
+                    .resizable()
+                    .renderingMode(.template)
+                    .foregroundColor(.yellow)
+                    .accessibility(label: Text("Remove from favorites"))
+            } else {
+                Image("star-empty")
+                    .resizable()
+                    .renderingMode(.template)
+                    .foregroundColor(.gray)
+                    .accessibility(label: Text("Add to favorites"))
+            }
+        }
+        .frame(width: 20, height: 20)
+        .buttonStyle(PlainButtonStyle())
+    }
+
+    func toggleFavoriteStatus() {
+        userData.landmarks[self.landmarkIndex].isFavorite.toggle()
+    }
+
     var body: some View {
         ScrollView {
             MapView(coordinate: landmark.locationCoordinate)
                 .frame(height: 250)
                 .overlay(
                     GeometryReader { geometry in
-                        Button("Open in Maps") {
-                            let destination = MKMapItem(placemark: MKPlacemark(coordinate: self.landmark.locationCoordinate))
-                            destination.name = self.landmark.name
-                            destination.openInMaps()
-                        }
-                        .frame(width: geometry.size.width, height: geometry.size.height, alignment: .bottomTrailing)
-                        .offset(x: -10, y: -10)
+                        self.openInMapsButton
+                            .frame(
+                                width: geometry.size.width,
+                                height: geometry.size.height,
+                                alignment: .bottomTrailing
+                            )
+                            .offset(x: -10, y: -10)
                     }
                 )
 
@@ -38,35 +74,16 @@ struct NavigationDetail: View {
                     CircleImage(image: landmark.image.resizable(), shadowRadius: 4)
                         .frame(width: 160, height: 160)
 
-                    VStack(alignment: .leading) {
+                    VStack(alignment: .leading, spacing: 2) {
                         HStack {
                             Text(landmark.name).font(.title)
 
-                            Button(action: {
-                                self.userData.landmarks[self.landmarkIndex]
-                                    .isFavorite.toggle()
-                            }) {
-                                if userData.landmarks[self.landmarkIndex].isFavorite {
-                                    Image("star-filled")
-                                        .resizable()
-                                        .renderingMode(.template)
-                                        .foregroundColor(.yellow)
-                                        .accessibility(label: Text("Remove from favorites"))
-                                } else {
-                                    Image("star-empty")
-                                        .resizable()
-                                        .renderingMode(.template)
-                                        .foregroundColor(.gray)
-                                        .accessibility(label: Text("Add to favorites"))
-                                }
-                            }
-                            .frame(width: 20, height: 20)
-                            .buttonStyle(PlainButtonStyle())
+                            self.favoriteButton
                         }
                         Text(landmark.park)
-                        Text(landmark.state)
+                        Text(landmark.state).font(.caption)
                     }
-                    .font(.caption)
+
                 }
 
                 Divider()
@@ -87,5 +104,6 @@ struct NavigationDetail_Previews: PreviewProvider {
     static var previews: some View {
         NavigationDetail(landmark: landmarkData[0])
             .environmentObject(UserData())
+            .previewLayout(.fixed(width: 600, height: 500))
     }
 }
